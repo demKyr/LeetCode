376aed7da30fd5da8f3f0438517111eb743569fb
Demetris Kyriacou <64605968+demKyr@users.noreply.github.com>
2025-11-03 18:26:51 +0000

    Time: 0 ms (100%), Space: 10.2 MB (72.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,30 @@
+// We only need to store the value of the best solutions up to i-1 and i-2
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        vector <int> solu (n);
-        solu[0] = nums[0];
-        solu[1] = max(nums[0],nums[1]);
+        int prevprev = nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        int prev = max(nums[0],nums[1]);
+        int cur;
         for(int i=2;i<n;i++){
-            solu[i] = max(solu[i-1], solu[i-2] + nums[i]);
+            cur = max(prev, prevprev + nums[i]);
+            prevprev = prev;
+            prev = cur;
         }
-        return solu[n-1];
+        return cur;
+
+        // int n = nums.size();
+        // if(n==1) return nums[0];
+        // vector <int> solu (n);
+        // solu[0] = nums[0];
+        // solu[1] = max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++){
+        //     solu[i] = max(solu[i-1], solu[i-2] + nums[i]);
+        // }
+        // return solu[n-1];
     }
 };
 
